437220593cc4a8de05bbcf78e0308d0b0d197557
Stas Litvinov <Stas.Litvinov@gmail.com>
2025-04-20 15:49:43 +0200

    init project

---
+++ StaLiPizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StaLiPizza
+//
+//  Created by Stanislav Litvinov on 20.4.25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StaLiPizza/StaLiPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaLiPizzaApp.swift
+//  StaLiPizza
+//
+//  Created by Stanislav Litvinov on 20.4.25..
+//
+
+import SwiftUI
+
+@main
+struct StaLiPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
